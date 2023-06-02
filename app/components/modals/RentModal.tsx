4bc423eaa25592b2df0f9d6af340959ffcb1855a
LilahDim <frostbitezebra421@gmail.com>
2023-06-02 12:16:36 -0700

    added listing card display to the main page and favoriting options

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,20 +1,22 @@
 "use client";
 
-import Modal from "./Modal";
-import useRentModal from "@/app/hooks/useRentModal";
-import { useMemo, useState } from "react";
-import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
-import Heading from "../Heading";
-import { categories } from "../navbar/Categories";
-import CategoryInput from "../inputs/CategoryInput";
-import CountrySelect from "../inputs/CountrySelect";
-import dynamic from "next/dynamic";
-import Counter from "../inputs/Counter";
-import ImageUpload from "../inputs/ImageUpload";
-import Input from "../inputs/Input";
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
+import { useMemo, useState } from "react";
+
+import useRentModal from "@/app/hooks/useRentModal";
+
+import Modal from "./Modal";
+import Counter from "../inputs/Counter";
+import CategoryInput from "../inputs/CategoryInput";
+import CountrySelect from "../inputs/CountrySelect";
+import { categories } from "../navbar/Categories";
+import ImageUpload from "../inputs/ImageUpload";
+import Input from "../inputs/Input";
+import Heading from "../Heading";
 
 enum STEPS {
   CATEGORY = 0,
@@ -26,10 +28,11 @@ enum STEPS {
 }
 
 const RentModal = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const rentModal = useRentModal();
-  const [step, setStep] = useState(STEPS.CATEGORY);
   const router = useRouter();
+  const rentModal = useRentModal();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [step, setStep] = useState(STEPS.CATEGORY);
 
   const {
     register,
@@ -52,8 +55,8 @@ const RentModal = () => {
     },
   });
 
-  const category = watch("category");
   const location = watch("location");
+  const category = watch("category");
   const guestCount = watch("guestCount");
   const roomCount = watch("roomCount");
   const bathroomCount = watch("bathroomCount");
@@ -129,7 +132,16 @@ const RentModal = () => {
         title="Which of these best describes your place?"
         subtitle="Pick a category"
       />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
+      <div
+        className="
+          grid 
+          grid-cols-1 
+          md:grid-cols-2 
+          gap-3
+          max-h-[50vh]
+          overflow-y-auto
+        "
+      >
         {categories.map((item) => (
           <div key={item.label} className="col-span-1">
             <CategoryInput
@@ -149,11 +161,11 @@ const RentModal = () => {
       <div className="flex flex-col gap-8">
         <Heading
           title="Where is your place located?"
-          subtitle="Help guests find you"
+          subtitle="Help guests find you!"
         />
         <CountrySelect
-          onChange={(value) => setCustomValue("location", value)}
           value={location}
+          onChange={(value) => setCustomValue("location", value)}
         />
         <Map center={location?.latlng} />
       </div>
@@ -165,27 +177,27 @@ const RentModal = () => {
       <div className="flex flex-col gap-8">
         <Heading
           title="Share some basics about your place"
-          subtitle="What amenitites do you have?"
+          subtitle="What amenitis do you have?"
         />
         <Counter
+          onChange={(value) => setCustomValue("guestCount", value)}
+          value={guestCount}
           title="Guests"
           subtitle="How many guests do you allow?"
-          value={guestCount}
-          onChange={(value) => setCustomValue("guestCount", value)}
         />
         <hr />
         <Counter
+          onChange={(value) => setCustomValue("roomCount", value)}
+          value={roomCount}
           title="Rooms"
           subtitle="How many rooms do you have?"
-          value={roomCount}
-          onChange={(value) => setCustomValue("roomCount", value)}
         />
         <hr />
         <Counter
-          title="Bathrooms"
-          subtitle="How many bathrooms do you have"
-          value={bathroomCount}
           onChange={(value) => setCustomValue("bathroomCount", value)}
+          value={bathroomCount}
+          title="Bathrooms"
+          subtitle="How many bathrooms do you have?"
         />
       </div>
     );
@@ -199,8 +211,8 @@ const RentModal = () => {
           subtitle="Show guests what your place looks like!"
         />
         <ImageUpload
-          value={imageSrc}
           onChange={(value) => setCustomValue("imageSrc", value)}
+          value={imageSrc}
         />
       </div>
     );
@@ -210,7 +222,7 @@ const RentModal = () => {
     bodyContent = (
       <div className="flex flex-col gap-8">
         <Heading
-          title="How would you describe your place"
+          title="How would you describe your place?"
           subtitle="Short and sweet works best!"
         />
         <Input
@@ -257,13 +269,14 @@ const RentModal = () => {
 
   return (
     <Modal
-      title="Airbnb your home"
-      onClose={rentModal.onClose}
-      onSubmit={handleSubmit(onSubmit)}
+      disabled={isLoading}
       isOpen={rentModal.isOpen}
+      title="Airbnb your home!"
       actionLabel={actionLabel}
+      onSubmit={handleSubmit(onSubmit)}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+      onClose={rentModal.onClose}
       body={bodyContent}
     />
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,9 @@ export default async function RootLayout({
           <RentModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
-        {children}
+        <div className="pb-20 pt-28">
+          {children}
+          </div>
       </body>
     </html>
   );
